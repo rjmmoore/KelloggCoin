9bad709b1f5da188fd45252c7aa8388c8cc89e3a
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-02-03 22:08:33 +0000

    Completed practice exercise

--- practice.rb
+++ practice.rb
@@ -26,22 +26,52 @@ blockchain = [
 # Evan's KelloggCoin balance is 10350
 # Anthony's KelloggCoin balance is 2650
 
-# 👇👇👇 Your code HERE 👇👇👇
-
-# Users_list = []'Ben', 'Brian', 'Evan', 'Anthony']
 
 wallets = []
 
 for i in 0...blockchain.length
-  from_user = blockchain[i] ["from_user"]
+  from_user = blockchain[i]["from_user"]
   to_user = blockchain[i]["to_user"]
+
+  found_from = false
+  found_to = false
+
+  for j in 0...wallets.length
+    if wallets[j]["user"] == from_user
+      found_from = true
+    end
+    if wallets[j]["user"] == to_user
+      found_to = true
+    end
+  end
+
+  wallets << { "user" => from_user, "balance" => 0 } if from_user && !found_from
+  wallets << { "user" => to_user, "balance" => 0 } if !found_to
 end
 
+for i in 0...blockchain.length
+  from_user = blockchain[i]["from_user"]
+  to_user = blockchain[i]["to_user"]
+  amount = blockchain[i]["amount"]
+
+  if from_user
+    for j in 0...wallets.length
+      if wallets[j]["user"] == from_user
+        wallets[j]["balance"] -= amount
+        break
+      end
+    end
+  end
+
+  for j in 0...wallets.length
+    if wallets[j]["user"] == to_user
+      wallets[j]["balance"] += amount
+      break
+    end
+  end
+end
 
 puts "Wallet Balances:"
 for i in 0...wallets.length
   puts "#{wallets[i]['user']}: #{wallets[i]['balance']} Kelloggcoins"
 end
-
-
-
